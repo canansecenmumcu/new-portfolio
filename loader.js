80c82091e51604c544cc0bb50e64d165e707dc5e
canansecenmumcu <canansecenmumcu@gmail.com>
2024-09-25 23:30:34 +0300

    fifth

--- loader.js
+++ loader.js
@@ -2,7 +2,10 @@
 
 export default function loader({ src, width, quality }) {
   const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
-  if (src.startsWith("images.pexels.com")) return src;
-  /* return `https://nextjsportfolio.com/${params.join(",")}${src}`; */
-  return `https://github.com/canansecenmumcu/new-portfolio.git/${params.join(",")}${src}`;
+
+  // Eğer harici bir kaynaktan resimse, doğrudan URL'yi döndür
+  if (src.startsWith("https://images.pexels.com")) return src;
+
+  // Public klasöründeki resimleri çekmek için doğru URL yapısı
+  return `${src}?${params.join(",")}`;
 }
