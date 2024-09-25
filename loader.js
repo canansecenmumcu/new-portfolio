5afd122781304a966ac46bb3a78cf81e1cf25957
canansecenmumcu <canansecenmumcu@gmail.com>
2024-09-25 20:58:13 +0300

    forth

--- loader.js
+++ loader.js
@@ -1,7 +1,8 @@
 "use client";
 
-export default function cloudinaryLoader({ src, width, quality }) {
+export default function loader({ src, width, quality }) {
   const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
   if (src.startsWith("images.pexels.com")) return src;
-  /* return `new-portfolio-cnn.vercel.app/${params.join(",")}${src}`; */
+  /* return `https://nextjsportfolio.com/${params.join(",")}${src}`; */
+  return `https://github.com/canansecenmumcu/new-portfolio.git/${params.join(",")}${src}`;
 }
